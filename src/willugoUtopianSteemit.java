a3099cad2d9637fe99444137b3b5ef77a564e4ef
will-ugo <34867853+will-ugo@users.noreply.github.com>
2017-12-28 14:06:53 +0100

    Update willugoUtopianSteemit.java

--- src/willugoUtopianSteemit.java
+++ src/willugoUtopianSteemit.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-
+```
 public class willugoUtopianSteemit {
 	public static void main (String args []) {
 		String firstNumber;
@@ -7,12 +7,14 @@ public class willugoUtopianSteemit {
 		int num1;
 		int num2;
 		int sum;
+```
 		
 		firstNumber = JOptionPane.showInputDialog("Enter the First Number");
 		if (firstNumber == null) {
 			JOptionPane.showMessageDialog(null, "You have quit the program");
 			System.exit(0);
 		}
+
 		else {
 			secondNumber = JOptionPane.showInputDialog("Enter the Second Number");
 			if (secondNumber == null) {
