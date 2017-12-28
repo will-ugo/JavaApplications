cb299aa5837f265659709b7b4f5fce56a210319e
will-ugo <34867853+will-ugo@users.noreply.github.com>
2017-12-28 10:54:27 +0100

    First Commit

---
+++ src/willugoUtopianSteemit.java
@@ -0,0 +1,33 @@
+import javax.swing.JOptionPane;
+
+public class willugoUtopianSteemit {
+	public static void main (String args []) {
+		String firstNumber;
+		String secondNumber;
+		int num1;
+		int num2;
+		int sum;
+		
+		firstNumber = JOptionPane.showInputDialog("Enter the First Number");
+		if (firstNumber == null) {
+			JOptionPane.showMessageDialog(null, "You have quit the program");
+			System.exit(0);
+		}
+		else {
+			secondNumber = JOptionPane.showInputDialog("Enter the Second Number");
+			if (secondNumber == null) {
+				JOptionPane.showMessageDialog(null, "You have quit the program");
+				System.exit(0);
+			}
+			else {
+				num1 = Integer.parseInt(firstNumber);
+				num2 = Integer.parseInt(secondNumber);
+				
+				sum = num1 + num2;
+				JOptionPane.showMessageDialog(null, "The sum of both numbers is " + sum);
+			}
+		}
+	}
+}
+
+
